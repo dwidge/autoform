ef6ada986f3d8aad8116ea3e77ff9f783e077bec
dwidge <dwidge+gh@gmail.com>
2023-05-31 12:11:00 +0200

    rename js tsx

--- lib/AutoTable.tsx
+++ lib/AutoTable.tsx
@@ -2,10 +2,10 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-export const calcColumns = (schema) =>
+export const calcColumns = (schema: { [k: string]: any }) =>
   Object.entries(schema)
-    .filter(([k, v]) => k !== "id")
-    .map(([k, v]) => ({
+    .filter(([k]: [k: string, v: any]) => k !== "id")
+    .map(([k]: [k: string, v: any]) => ({
       field: k,
       headerName: k,
       width: 200,
